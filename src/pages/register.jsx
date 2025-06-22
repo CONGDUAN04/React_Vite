d27d23af4cd45dc209a718a544405374a85903de
Cong Duan <phamcongduannn@gmail.com>
2025-06-23 00:14:29 +0700

    Hoàn Thành Register

--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -1,4 +1,103 @@
+// Regx and check for email and phone number
+import { Button, Col, Form, Input, notification, Row } from "antd";
+import { registerUserAPI } from "../services/api.service";
+import { useNavigate } from "react-router-dom";
 const RegisterPage = () => {
-  return <div>Register Page</div>;
+  const [form] = Form.useForm();
+  const navigate = useNavigate();
+  const onFinish = async (values) => {
+    console.log(">>>check value:", values);
+    //// Call API to register user
+    const res = await registerUserAPI(
+      values.fullName,
+      values.email,
+      values.password,
+      values.phone
+    );
+    if (res.data) {
+      notification.success({
+        message: "Register User",
+        description: "ĐĂNG KÝ USER THÀNH CÔNG",
+      });
+      navigate("/login");
+    } else {
+      notification.error({
+        message: "Error register user",
+        description: JSON.stringify(res.message),
+      });
+    }
+  };
+  return (
+    <Form
+      form={form}
+      layout="vertical"
+      name="basic"
+      onFinish={onFinish}
+      style={{ margin: "10px" }}
+      // onFinishFailed={onFinishFailed}
+    >
+      <Row justify={"center"}>
+        <Col xs={24} md={6}>
+          <Form.Item
+            label="Full Name"
+            name="fullName"
+            rules={[{ required: true, message: "Please input your username!" }]}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Row justify={"center"}>
+        <Col xs={24} md={6}>
+          <Form.Item
+            label="Email"
+            name="email"
+            rules={[{ required: true, message: "Please input your email!" }]}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Row justify={"center"}>
+        <Col xs={24} md={6}>
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[{ required: true, message: "Please input your password!" }]}
+          >
+            <Input.Password />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Row justify={"center"}>
+        <Col xs={24} md={6}>
+          <Form.Item
+            label="Phone Number"
+            name="phone"
+            rules={[
+              {
+                required: true,
+                pattern: new RegExp(/\d+/g),
+                message: "Wrong format!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+        </Col>
+      </Row>
+
+      <Row justify={"center"}>
+        <Col xs={24} md={6}>
+          <Button onClick={() => form.submit()} type="primary">
+            Register
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  );
 };
 export default RegisterPage;
